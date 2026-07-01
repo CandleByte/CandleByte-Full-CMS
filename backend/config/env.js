51c1125445e6a485c8769b254368cce42bba2d7b
gulfems <75295522+gulfems@users.noreply.github.com>
2026-07-01 17:28:58 +0300

    first message

---
+++ backend/config/env.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+if (!process.env.JWT_SECRET) {
+    throw new Error('JWT SECRET IS MOT DEFINED IN .env FILE');
+}
+
+const env = {
+    PORT: process.env.PORT || '5000',
+    JWT_SECRET: process.env.JWT_SECRET
+}
+
+export default env;
+
