502d5bd5ae5fca2283c6d1db823eb8338a79eba1
gulfems <75295522+gulfems@users.noreply.github.com>
2026-07-03 23:04:45 +0300

    Auth middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import env from '../config/env.js';
+
+const auth = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Access denied. No token provided.' });
+    }
+
+    try {
+        const decoded = jwt.verift(token, env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: 'Invalid or expired token.' });
+    }
+}
+
+export default auth;
+
---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,5 @@
+import bcrypt from "bcryptjs";
+import env from "../config/env.js";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
