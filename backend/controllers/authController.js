90004b44409a745983a0854d71ab57a0197d8fb9
gulfems <75295522+gulfems@users.noreply.github.com>
2026-07-03 23:51:07 +0300

    import fix

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs';
 import User from '../models/User.js';
 import jwt from 'jsonwebtoken';
-
+import env from '../config/env.js';
 
 export const register = async (req, res) => {
 
@@ -39,26 +39,31 @@ export const login = async (req, res) => {
 
     const { loginInput, password } = req.body;
 
-    const isEmail = loginInput.includes('@');
+    try {
 
-    const query = {};
-    if (isEmail) {
-        query.email = loginInput;
-    } else {
-        query.username = loginInput;
-    }
+        const isEmail = loginInput.includes('@');
 
-    const user = await User.findOne(query);
-
-    if (user) {
-        const passwordCheck = await bcrypt.compare(password, user.password);
-        if (passwordCheck) {
-            const token = jwt.sign({ userId: user._id }, env.JWT_SECRET, { expiresIn: '7d' });
-            res.status(200).json({ token });
+        const query = {};
+        if (isEmail) {
+            query.email = loginInput;
         } else {
-            res.status(401).json({message: "Invalid email, username or password."});
+            query.username = loginInput;
         }
-    } else {
-        res.status(401).json({message: "Invalid email, username or password."});
+
+        const user = await User.findOne(query);
+
+        if (user) {
+            const passwordCheck = await bcrypt.compare(password, user.password);
+            if (passwordCheck) {
+                const token = jwt.sign({ userId: user._id }, env.JWT_SECRET, { expiresIn: '7d' });
+                res.status(200).json({ token });
+            } else {
+                res.status(401).json({ message: "Invalid email, username or password." });
+            }
+        } else {
+            res.status(401).json({ message: "Invalid email, username or password." });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Server error." });
     }
 }
