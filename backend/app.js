90ba0e08314fe538e3ef58e0a5b076bba3ac1ff5
gulfems <75295522+gulfems@users.noreply.github.com>
2026-07-04 21:11:53 +0300

    last changes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes.js';
+import auth from './middleware/auth.js';
 
 const app = express();
 
@@ -8,6 +9,9 @@ app.get('/', (req, res) => {
     res.send({ status: "ok" });
 });
 app.use('/api/auth', authRoutes);
+app.get('/api/protected', auth, (req, res) => {
+    res.send({ message: "This is a protected route.", user: req.user });
+});
 
 export default app;
 
