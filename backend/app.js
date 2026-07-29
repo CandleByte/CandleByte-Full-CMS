ea4278a29deb277ad9a3d914271e8bcf7dfc0ea7
gulfems <75295522+gulfems@users.noreply.github.com>
2026-07-29 15:00:45 +0300

    Everything works successfully projects + auth

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes.js';
 import auth from './middleware/auth.js';
+import projectRoutes from './routes/projectRoutes.js';
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.get('/', (req, res) => {
     res.send({ status: "ok" });
 });
 app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
 app.get('/api/protected', auth, (req, res) => {
     res.send({ message: "This is a protected route.", user: req.user });
 });
