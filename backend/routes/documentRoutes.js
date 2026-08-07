8dc03418205861238822224443760a8481e5ca44
gulfems <75295522+gulfems@users.noreply.github.com>
2026-08-08 02:32:59 +0300

    file upload works!!! yessss

--- backend/routes/documentRoutes.js
+++ backend/routes/documentRoutes.js
@@ -1,16 +1,15 @@
 import express from 'express';
 import auth from '../middleware/auth.js';
 import { createDocument, getDocument, updateDocument, getDocumentsByProject, deleteDocument } from '../controllers/documentController.js';
+import { upload } from '../middleware/upload.js';
 
 const router = express.Router();
 router.use(auth);
 
-router.post('/', createDocument);
+router.post('/', upload.single('file'), createDocument);
 router.get('/project/:projectId', getDocumentsByProject);
 router.get('/:id', getDocument);
-router.put('/:id', updateDocument);
+router.put('/:id', upload.single('file'), updateDocument);
 router.delete('/:id', deleteDocument);
 
 export default router;
-
-
